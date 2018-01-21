8f4c59f228494057788eae09361d7d0cabe7bf24
Tomasz <estewez@gmail.com>
2018-01-21 12:36:34 +0100

    Task completed

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '634792442633-9ihcuv85vtps7hnga1qc3bbm580ccllj.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'nLTw0wIABQVulSBsJa4dm8XF',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
